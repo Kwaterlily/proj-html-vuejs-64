ab7700c48bc67fb1473c1fd4d644e1060ad46277
liviaAugusta <liviamattoni@gmail.com>
2022-06-17 14:27:53 +0200

    inizio struttura Header e Footer (senza css)

---
+++ src/components/AvadaFooter.vue
@@ -0,0 +1,70 @@
+<template>
+    
+      <section>
+           <div>
+               <img src="../assets/images/avada-drivers-logo-2x-200x39.png" alt="footer-ad-grid">
+           </div> 
+          <div>
+              <p>Avada Driving School the right choice for experienced drivers looking foa a refresher course. or the brand new driver wanting to pass first time.</p>
+         </div>   
+          <nav>
+              <ul>
+                  <li><a href="#"></a>12345 North Main Street, New York, NY 555555</li>
+                  <li><a href="#"></a>1.800.555.6789</li>
+                  <li><a href="#"></a>info@your-domain.com</li>
+                  <li><a href="#"></a>Theme.Fusion.com</li>
+              </ul>
+              <ul>
+                  <li><a href="#"></a>Pass Plus</li>
+                  <li><a href="#"></a>Intensive Couse</li>
+                  <li><a href="#"></a>Auutomatic</li>
+                  <li><a href="#"></a>Instructor Training</li>
+              </ul>
+          </nav>
+          <div>
+              <img src="../assets/images/footer-ad-grid-400x357.png" alt="footer-ad-grid"><br>
+              <button type="button">Book Now</button>
+          </div>  
+      </section>
+
+  </template>
+  
+  <script>
+  export default {
+    name: 'AvadaFooter',
+    props: {
+      msg: String
+    }
+  }
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped lang="scss">
+
+  section {
+      text-align: center;
+      background-image: url("../assets/images/footer-background-color.jpg");
+      background-repeat: no-repeat;
+
+
+      nav {
+
+
+          ul {
+
+
+              li {
+
+
+                  a {
+                    
+                  }
+              }
+          }
+      }
+  
+  
+  }   
+  
+ 
+  </style>
---
+++ src/components/AvadaHeader.vue
@@ -0,0 +1,79 @@
+<template>
+  <header>
+      <nav>
+        <ul>
+           <li><a href="#">Comics</a></li>
+           <li><a href="#">Movies</a></li>
+        </ul>
+        
+         <ul>
+           <li><a href="#"><img alt="Avada logo" src="../assets/images/avada-drivers-logo-1x.png"></a></li>
+           <li><a href="#">Characters</a></li>
+           <li><a href="#">Comics</a></li>
+           <li><a href="#">Movies</a></li>
+           <li><a href="#">Tv</a></li>
+           <li><a href="#">Games</a></li>
+           <li><a href="#">Collectibles</a></li>
+           <li><a href="#">Videos</a></li>
+           <li><a href="#">Fans</a></li>
+           <li><a href="#">News</a></li>
+           <li><a href="#">Shop</a></li>
+           <li><button type="button">Book Now</button></li>  
+         </ul>
+      </nav>
+    </header>
+</template>
+
+<script>
+export default {
+  name: 'AvadaHeader',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+header {
+  text-align: center;
+
+
+  ul {
+    list-style-type: none;
+    margin: 20px 0;
+    text-align: center;
+    
+    
+
+    li {
+      display: inline-block;
+      padding: 10px;
+      
+
+      a {
+        text-decoration: none;
+        color: #1C1C1C;
+        text-transform: uppercase;
+        font-size: 0.7em;
+
+        img {
+          text-align: start;
+        }
+        
+
+        &:hover {
+          color: #0082F9;
+        }
+
+        &:active {
+          background-color: #0082F9;
+        }
+      }
+
+    }
+  }
+}
+
+</style>
